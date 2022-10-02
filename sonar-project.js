77add314917c0a5acb0a6c20898d83f0fde40179
BRENDALINE22 <111323981+BRENDALINE22@users.noreply.github.com>
2022-10-02 16:48:46 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.84.55.60:9000/',
+  serverUrl: 'http://172.31.93.247:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': 'd577b0668dce76e8e0ccd1b3b88865790b9aae96',
+	    'sonar.login': '48be5707165bb6647fa130ea96dbc6b0e40f4c08',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
